2f09d2233bd91902e0e3a2b909cdab63b5a574ad
kaberyo <40202547+kaberyo@users.noreply.github.com>
2018-07-18 19:25:52 +0900

    fix

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,7 +5,7 @@ $(function() {
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     search_list.append(html)
   }
@@ -16,11 +16,11 @@ $(function() {
       $("#user-search-result").append(html)
   }
 
-  function addUser(user){
+  function addUser(name, id){
     var member_count =$(".js-chat-member").length;
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${member_count}'>
-                <input name='group[user_ids][]' type='hidden' value='${user.attr('data-user-id')}'>
-                <p class='chat-group-user__name'>${user.attr('data-user-name')}</p>
+                <input name='group[user_ids][]' type='hidden' value='${id}'>
+                <p class='chat-group-user__name'>${name}</p>
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
     group_user_list.append(html)
@@ -55,15 +55,15 @@ $(function() {
     }
   });
 
-  $("#user-search-result").on('click', '.user-search-add', function() {
-  addUser($(this));
+  $("#user-search-result").on('click', '.js-add-btn', function() {
+    var name = $(this).data("user-name")
+    var id = $(this).data("user-id")
+    addUser(name, id);
+    $(this).parent().remove();
   });
 
   $("#chat-group-users").on("click", '.js-remove-btn', function() {
     $(this).parent().remove();
   })
 
-  $("#user-search-result").on("click", '.js-add-btn', function() {
-    $(this).parent().remove();
-  })
 });
