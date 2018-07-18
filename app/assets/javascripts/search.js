ccffa781aff2ee1d32c5261d04829a7666366c39
kaberyo <40202547+kaberyo@users.noreply.github.com>
2018-07-18 18:16:41 +0900

    fix

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,7 +3,6 @@ $(function() {
   var group_user_list = $("#chat-group-users")
 
   function appendUser(user){
-    console.log(user.id)
     var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -25,12 +24,10 @@ $(function() {
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
     group_user_list.append(html)
-    console.log()
   }
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input)
 
     $.ajax({
       type: 'GET',
@@ -39,7 +36,6 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users){
-      console.log(users)
       $("#user-search-result").empty();
       if(users.length !== 0){
         users.forEach(function(user){
