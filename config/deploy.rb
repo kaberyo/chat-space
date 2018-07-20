7cfd410fef39ab31c2c81bb090ccdc3efed9f04d
kaberyo <40202547+kaberyo@users.noreply.github.com>
2018-07-20 16:42:21 +0900

    edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/ryotakabe/.ssh
-/keypair.pem']
+                  keys: '/Users/ryotakabe/.ssh
+/keypair.pem'
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
