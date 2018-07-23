172512acfb17dd14047112d3d5f06c8c73e83ee5
kaberyo <40202547+kaberyo@users.noreply.github.com>
2018-07-23 18:56:03 +0900

    edit

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,6 +1,7 @@
 module ControllerMacros
   def login(user)
-    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env["
+      devise.mapping"] = Devise.mappings[:user]
     sign_in user
   end
 end
