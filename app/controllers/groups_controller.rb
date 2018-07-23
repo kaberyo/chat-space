cde9aad362dd4db6ee6a9ab0de081f78bdabd6f0
kaberyo <40202547+kaberyo@users.noreply.github.com>
2018-07-23 17:15:37 +0900

    fixed rubocop

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
-  def index
-  end
+  def index; end
 
   def new
     @group = Group.new
@@ -17,8 +16,7 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @group.update(group_params)
